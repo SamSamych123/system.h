61b3ef9e00f78eb78da0dfd761583bb03945d8df
SamSamych123 <varibrus.alex13@gmail.com>
2026-05-25 22:03:11 +0700

    Update system.h

--- system.h
+++ system.h
@@ -15,11 +15,11 @@
   #include <windows.h>
   #include <shellapi.h>
   #include <processthreadsapi.h>
-  #define STAT_FUNC stat
+  //#define STAT_FUNC stat
 #elif defined(__linux__) || defined(__APPLE__) || defined(__unix__) || defined(__ANDROID__)
   #include <unistd.h>
   #include <sys/wait.h>
-  #define STAT_FUNC stat
+  //#define STAT_FUNC stat
 //#elif defined(__SamOS__)
   //#include <samos/system.h>
   //system("#okak");
@@ -27,20 +27,23 @@
   #warning "You are using an operating system that is not supported by this library because there is no suitable library for you (although you can just skip this)"
 #endif
 
-#define VERSION_MAGOR 0
+#define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 #define VERSION_TESTING 1
 
-#warning "This is test version, and functions is not stable"
+#if VERSION_TESING == 1
+  #warning "This is test version, and functions is not stable"
+#endif
 int arch(void);
 int os(void);
+int type_os(void);
 int comp(void);
 //int lang(void);
-void delay(double millisecond);
+void delay(double second);
 /*void command(char *main_command, size_t buf_size, const char *argument1, const char *argument2, const char *argument3, const char *argument4, const char *argument5, const char *argument6);*/
 void command(char *commandd);
 void msg_print(int level, const char *msg, int action);
-void init_cpuinfo(void);
+//void init_cpuinfo(void);
 void clear_screen(void);
 int conf_user(const char *msg, int choise);
 void name_user(char *save_name);
@@ -49,10 +52,10 @@ void password_user(char *save_password);
 //void remove(const char *full_path_to_remove, int o);
 int verification_name_user(const char *name);
 int verification_password_user(const char *password);
-void print_with_delay(const char *text, int sec);
+void print_with_delay(const char *text, double sec);
 
-unsigned char inited_cpuinfo = 0;
-unsigned char error_out = 0;
+//extern unsigned char inited_cpuinfo;
+//extern unsigned char error_out;
 
 #define CRITICAL_ERR -2
 #define ERR -1
